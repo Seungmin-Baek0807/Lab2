1ceaa556f9bc9559b234aa4c6fe263935a0baa5e
seungminbaek <seungmin0807@snu.ac.kr>
2024-06-07 08:19:28 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -3,20 +3,28 @@
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+	if (cnt < M) {
+		cnt++;
+		for (int i = 0; i < N; i++) {
+			vec.push_back(i + 1);
+			func(cnt);
+			vec.pop_back();
+		}
+		
+	}
+	else {
+		for (int i = 0; i < M; i++) {
+			cout << vec[i] << " ";
+		}
+		cout << endl;
+	}
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+	cin >> N >> M;
+	func(0);
 }
